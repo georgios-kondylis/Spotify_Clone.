653327c484a49dca43675de93969723e9a9d3c44
Georgios <georgios.p.kondylis@gmail.com>
2025-02-02 15:05:55 +0100

    fixed the redirect url

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,8 @@ export const authEndpoint = 'https://accounts.spotify.com/authorize';
 export const clientId = import.meta.env.VITE_CLIENT_ID;
 export const liveURL = import.meta.env.VITE_LIVE_URL;
 export const devURL = 'http://localhost:5173/';
-export const redirectURL = import.meta.env.PROD === 'production' ? liveURL : devURL;
+export const redirectURL = import.meta.env.PROD ? liveURL : devURL;
+console.log(redirectURL);
 export const scopes = [
 	'playlist-read-collaborative',
 	'playlist-modify-public',
